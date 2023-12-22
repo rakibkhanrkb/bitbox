8821c8da821a7b78ed857e4f5dc3fddf40cc9fdc
rakib <bitboxbd580@gmail.com>
2023-12-22 12:39:05 +0600

    bitbox app, first commit

--- bitbox/admin.py
+++ bitbox/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import reg
+from .models import reg,bitboxc
 
 
 
 admin.site.register(reg)
+admin.site.register(bitboxc)
--- bitbox/views.py
+++ bitbox/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import reg
+from .models import reg, bitboxc
 
 def index(request):
-	inf= reg.objects.filter()
+	inf= bitboxc.objects.filter()
 	return render(request,'bitbox/home.html',{'inf': inf})
 
 
@@ -11,6 +11,6 @@ def search_product(request):
     if request.method == "POST":
         query_phone = request.POST['searched']
         if query_phone:	
-            results = reg.objects.filter(phone__contains=query_phone)
+            results = bitboxc.objects.filter(phonenumber__contains=query_phone)
             return render(request, 'bitbox/search.html', {"results":results})
     return render(request, 'bitbox/search.html')
