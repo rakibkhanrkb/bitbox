5d4d68fc33f8ff516eb2136f369d6fd514bf6672
Rakib <54811075+rakibkhanrkb@users.noreply.github.com>
2026-03-25 11:33:52 +0600

    Update static file settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
+]
